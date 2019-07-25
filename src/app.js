9f6eb7ad784d0f7589716fcbcffe9e4375029281
Wes Jones <wesjones@Wess-MBP.fios-router.home>
2019-07-25 16:29:45 -0400

    moved components to components folder

--- src/app.js
+++ src/app.js
@@ -1,13 +1,4 @@
-// import './utils.js';
-// import subtract, { square, add } from './utils.js';
-
-// console.log("app.js is running!!!!");
-// console.log(square(4));
-// console.log(add(100, 20));
-// console.log(subtract(4,2));
-
-
-import isSenior, { isAdult, canDrink } from './person.js';
-console.log(isAdult(21));
-console.log(canDrink(21));
-console.log(isSenior(90));
+import React from 'react';
+import ReactDOM from 'react-dom';
+import IndecisionApp from './components/IndecisionApp'
+ReactDOM.render(<IndecisionApp  />, document.getElementById('app'));
