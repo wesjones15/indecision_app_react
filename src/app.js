5f6cce59b42e7ae4319367a5a08b5a213684eca9
Wes Jones <wesjones@Wess-MacBook-Pro.local>
2019-07-30 12:21:48 -0400

    committing before trying new new class structure

--- src/app.js
+++ src/app.js
@@ -1,4 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import IndecisionApp from './components/IndecisionApp'
+import IndecisionApp from './components/IndecisionApp';
+
 ReactDOM.render(<IndecisionApp  />, document.getElementById('app'));
+
+class OldSyntax {
+    constructor() {
+        this.name = 'Mike';
+    }
+}
+
+const oldSyntax = new OldSyntax();
+console.log(oldSyntax);
+
+//----------------
+
+class NewSyntax {
+    name = 'jen';
+    
+}
+const newSyntax = new NewSyntax();
+console.log(newSyntax);
